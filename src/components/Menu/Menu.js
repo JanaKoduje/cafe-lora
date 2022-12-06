14a9a6e879ff8402b6555c19cfe3298b58a3e8e7
JanaKoduje <jjanule@gmail.com>
2022-12-06 18:22:55 +0100

    ziskani dat z api

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,31 +3,26 @@ import { Drink } from "./Drink";
 import "./../../style.css";
 import "./../../index.css";
 
-export const Menu = () => {
+export const Menu = ({ drinks }) => {
   const element = document.createElement("section");
   element.setAttribute("id", "menu");
   element.classList.add("menu");
 
   const drinkList = document.createElement("div");
   drinkList.classList.add("drinks-list");
-  drinkList.append(
-    Drink({
-      id: 'romano',
-      name: 'Romano',
-      ordered: false,
-      image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-      layers: [
-        {
-          color: '#fbdf5b',
-          label: 'citrón',
-        },
-        {
-          color: '#613916',
-          label: 'espresso',
-        },
-      ]
+
+  if (drinks === undefined) {
+    fetch(`https://apps.kodim.cz/daweb/cafelora/api/me/drinks`, {
+      method: "GET",
+      headers: {
+        Authorization: `Email klamy@centrum.cz`,
+      },
     })
-  );
+      .then((response) => response.json())
+      .then((data) => element.replaceWith(Menu({ drinks: data.results })));
+  } else {
+    drinkList.append(...drinks.map((drink) => Drink(drink)));
+  }
 
   element.append(drinkList);
 
