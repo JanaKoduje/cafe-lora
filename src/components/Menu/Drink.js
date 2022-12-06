907ef7ee54a9f6cf456652de79b9046b6552bfde
JanaKoduje <jjanule@gmail.com>
2022-12-06 18:33:47 +0100

    pridana akce na tlacitko Objednat

--- src/components/Menu/Drink.js
+++ src/components/Menu/Drink.js
@@ -6,6 +6,11 @@ import { Layer } from "./Layer";
 export const Drink = (props) => {
   const { id, name, ordered, image, layers } = props;
 
+  let orderClass = "";
+  if (ordered) {
+    orderClass = "order-btn--ordered";
+  }
+
   const element = document.createElement("div");
   element.classList.add("drink");
 
@@ -19,8 +24,8 @@ export const Drink = (props) => {
      </div>
    </div>
    <div class="drink__controls">
-     <button class="order-btn">
-       Objednat
+     <button class="order-btn ${orderClass}">
+       ${ordered ? "Zrušit" : "Objednat"}
      </button>
    </div>
 `;
@@ -28,6 +33,19 @@ export const Drink = (props) => {
   element
     .querySelector(".drink__info")
     .append(...layers.map((layer) => Layer(layer)));
+
+  element.querySelector(".order-btn").addEventListener("click", () => {
+    fetch(`https://apps.kodim.cz/daweb/cafelora/api/me/drinks/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Email klamy@centrum.cz`,
+      },
+      body: JSON.stringify({ ordered: !ordered }),
+    })
+      .then((response) => response.json())
+      .then((data) => element.replaceWith(Drink(data.results)));
+  });
 
   return element;
 };
